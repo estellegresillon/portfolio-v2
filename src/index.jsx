a62f07e025dc5f188ff5ef1c0e70ccecfcdcc9f0
Estelle Gresillon <estelle.gresillon@kardinal.ai>
2022-09-08 18:12:27 +0200

    added smooth scroll

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import { hydrate, render } from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Lenis from "@studio-freight/lenis";
 
 import "./assets/index.css";
 import Menu from "./Menu";
 import Home from "./Home";
 
-const App = () => (
-  <BrowserRouter>
-    <React.Fragment>
-      <Menu />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-      </Routes>
-    </React.Fragment>
-  </BrowserRouter>
-);
+const lenis = new Lenis({
+  lerp: 0.1,
+  smooth: true,
+});
+
+const App = () => {
+  const scrollFn = () => {
+    lenis.raf();
+    requestAnimationFrame(scrollFn);
+  };
+  requestAnimationFrame(scrollFn);
+
+  return (
+    <BrowserRouter>
+      <React.Fragment>
+        <Menu />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </React.Fragment>
+    </BrowserRouter>
+  );
+};
 
 const rootElement = document.getElementById("root");
 
